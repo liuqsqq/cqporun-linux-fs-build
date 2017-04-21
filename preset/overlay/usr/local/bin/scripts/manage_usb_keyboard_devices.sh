b4250a25b97e5ecbc74b4cff7b1df311dcdaf047
liuqs <liuqs@cqporun.com>
2017-04-21 17:28:21 +0800

    fix usb-kerboard bug, fix updater bug

--- preset/overlay/usr/local/bin/scripts/manage_usb_keyboard_devices.sh
+++ preset/overlay/usr/local/bin/scripts/manage_usb_keyboard_devices.sh
@@ -8,6 +8,11 @@ keyword_mouse="mouse"
 keyboard=$(cat /proc/bus/input/devices | grep -i $keyword_keyboard | awk -F '"'  '{print $2}' | tail -1 | awk '{print $1}')
 
 if [ "$keyboard" != "" ];then
+	if [ $ACTION = "on" ];then
+		yinka-input $keyboard on
+	elif [ $ACTION = "off" ];then
+		yinka-input $keyboard off
+	fi
     echo $keyboard
 else
     echo "there is no keyboard"
@@ -16,16 +21,14 @@ fi
 mouse=$(cat /proc/bus/input/devices | grep -i $keyword_mouse | awk -F '"'  '{print $2}' | tail -1 | awk '{print $3}')
 
 if [ "$mouse" != "" ];then
+	sleep 1
+	if [ $ACTION = "on" ];then
+		yinka-input $mouse on
+	elif [ $ACTION = "off" ];then
+		yinka-input $mouse off
+	fi
     echo $mouse
 else
     echo "there is no mouse"
 fi
 
-if [ $ACTION = "on" ];then
-    yinka-input $keyboard on
-    yinka-input $mouse on
-elif [ $ACTION = "off" ];then
-    yinka-input $keyboard off
-    yinka-input $mouse off
-   
-fi
